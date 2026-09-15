a70e03db850d49bcdc0cfb420b95b5431c96c029
Le Duc Long <leduclong508@gmail.com>
2026-09-15 15:43:11 +0000

    dùng thuật toán quickShort thay cho sort

--- Code thiếu nhi/README.md
+++ Code thiếu nhi/README.md
@@ -20,6 +20,7 @@
         - **Độ phức tạp:**
             - **Time:** **O(N log N)**
                 - Hàm `std::sort` tốn $O(N \log N)$.
+                    - Dùng `swap, partition, quickShort algorithm` để thay hàm `std::sort`
                 - Vòng lặp `while` tốn $O(N)$ để quét mảng = 2 con trỏ.
                 
             - **Space:** **O(1)** 
@@ -27,3 +28,4 @@
         - **Bug / Ghi chú:**
             - **2 Pointers:** Kỹ thuật tìm kiếm cặp phần tử khi mảng **đã được sắp xếp**. Thay vì dùng 2 vòng for lồng nhau, thu hẹp phạm vi tìm, giảm thời gian tìm kiếm từ $O(N^2)$ xuống $O(N)$.
             - **Nhược điểm (Edge Case):** Lệnh Sort làm xáo trộn, làm **mất vị trí ban đầu (index)** của các phần tử. Nếu đề bài yêu cầu in ra *vị trí mảng gốc* thay vì *giá trị*, cách này không khả thi trừ khi dùng thêm cấu trúc dữ liệu lưu lại index trước khi sort. 
+
--- Code thiếu nhi/find_pair_sum.cpp
+++ Code thiếu nhi/find_pair_sum.cpp
@@ -3,6 +3,8 @@
     Topic: Find a pair in an array with a given sum
     12/09/2026: first edition
     13/09/2026: try another approach using sorting and 2 pointers
+    15/09/2026: using sorting without its library
+    - Using divide and conquer method to sort the array
 */
 
 // Brute force approach to find a pair in an array with a given sum
@@ -30,7 +32,8 @@ int main(){
 }
 */
 
-//Using Sorting 
+//Using Sorting with its library
+/*
 #include<stdio.h>
 #include<algorithm>
 
@@ -67,3 +70,71 @@ int main() {
     findPair(nums, n, target);
     return 0;
 }
+*/
+
+//Using Sorting without its library
+#include<stdio.h>
+
+void swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void partition(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = low -1;
+
+    for(int j = low; int j < high; j++) {
+        if(arr[j] <= pivot) {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+
+    swap(&arr[i + 1], &arr[high]);;
+    return i + 1;
+}
+
+void quickSort(int arr[], int low, int high) {
+    if(low < high) {
+        int pi = partition(arr, low, high);
+
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+void findPair(int arr[], int n, int sum) {
+    // Sorting the array in asscending order
+    quickSort(arr, 0, n - 1);
+
+    // Using 2 pointers to find the pair
+    int left = 0;
+    int right = n - 1;
+
+    // Loop until 2 pointers meet
+    while(left < right) {
+        if(arr[left] + arr[right] == sum) {
+            printf("Pair found: (%d, %d)\n", arr[left], arr[right]);
+            return;
+        }
+        // Increment left pointer if sum less than target
+        else if(arr[left] + arr[right] < sum) {
+            left++;
+        }
+        // Decrement right pointer if sum greater than target
+        else {
+            right--;
+        }
+    }
+    printf("No pair found\n");
+}
+
+int main() {
+    int nums[] = { 8, 7, 2, 5, 3, 1 };
+    int target = 10;
+    int n = sizeof(nums) / sizeof(nums[0]);
+    findPair(nums, n, target);
+    return 0;
+}
+
